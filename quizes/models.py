7044f6d19aecd9752597cfd24957724047b57ce1
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-06-26 15:31:55 +0200

    add so much stuff, but it's work !

--- quizes/models.py
+++ quizes/models.py
@@ -10,7 +10,7 @@ DIFF_CHOICES = (
 class Quiz(models.Model):
     name = models.CharField(max_length=120)
     topic = models.CharField(max_length=120)
-    number_of_questions = models.IntegerField
+    number_of_questions = models.IntegerField()
     time = models.IntegerField(help_text='duration of the quiz in minutes')
     required_score_to_pass = models.IntegerField(help_text='required score in %')
     difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES)
@@ -19,7 +19,7 @@ class Quiz(models.Model):
         return f"{self.name}-{self.topic}"
 
     def get_questions(self):
-        return self.question_set.all()
+        return self.question_set.all()[:self.number_of_questions]
 
     class Meta:
         verbose_name_plural = 'Quizes'
