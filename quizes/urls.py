8f81e7da9d264b7ef9a14875d67f63814946266b
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-06-27 10:26:47 +0200

    begin to work the quiz body

--- quizes/urls.py
+++ quizes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
     QuizListView,
-    quiz_view
+    quiz_view,
+    quiz_data_view,
 )
 
 app_name = 'quizes'
@@ -9,4 +10,5 @@ app_name = 'quizes'
 urlpatterns = [
     path('', QuizListView.as_view(), name='main-view'),
     path('<pk>', quiz_view, name='quiz-view'),
+    path('<pk>/data/', quiz_data_view, name='quiz-data-view'),
 ]
