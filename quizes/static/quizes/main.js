c3328f8d42104fdf08c0a6f2d6d0220823d61c99
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-06-27 10:12:42 +0200

    config modal body

--- quizes/static/quizes/main.js
+++ quizes/static/quizes/main.js
@@ -1,6 +1,29 @@
 const modalBtns = [...document.getElementsByClassName('modal-button')];
+const modalBody = document.getElementById('modal-body-confirm');
+const startBtn = document.getElementById('start-button');
 
 
 modalBtns.forEach(modalBtn => modalBtn.addEventListener('click', ()=> {
-    console.log(modalBtn)
+    const pk = modalBtn.getAttribute('data-pk');
+    const name = modalBtn.getAttribute('data-quiz');
+    const numQuestions = modalBtn.getAttribute('data-questions');
+    const difficulty = modalBtn.getAttribute('data-difficulty');
+    const scoreToPass = modalBtn.getAttribute('data-pass');
+    const time = modalBtn.getAttribute('data-time');
+
+    modalBody.innerHTML = `
+        <div class="h5 mb-3">Are tou sure you want to begin "<b>${name}</b>" ?</div>
+        <div class="text-muted">
+            <ul>
+                <li>Difficulty : <b>${difficulty}</b></li>
+                <li>Number of questions : <b>${numQuestions}</b></li>
+                <li>Score to pass : <b>${scoreToPass}%</b></li>
+                <li>Time : <b>${time} min</b></li>
+            </ul>
+        </div>
+    `
+
+    startBtn.addEventListener('click', () => {
+        console.log(window.location.href);
+    })
 }))
